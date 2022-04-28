cadb35105c6180b13d84b1035a449420286d2a73
uniK007 <kislayk970@gmail.com>
2022-04-28 12:39:08 +0530

    bet credit gets updated correctly when button clicked

--- src/components/BetCard.js
+++ src/components/BetCard.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Typography, Stack, ButtonGroup, Button, Box, createTheme, ThemeProvider } from "@mui/material";
 import PaidIcon from '@mui/icons-material/Paid';
 import "./BetCard.css";
@@ -15,12 +15,17 @@ const BetCard = () => {
 
     const [credit, setCredit] = useState(10000);
 
-    // const betHandler = () => {
-    //     const Hundred = document.getElementById('100')
-    //     const fiveHundred = document.getElementById('500')
-    //     const oneThousand = document.getElementById('1000')
+    const betHundred = () => {
+        setCredit(prevCredit => prevCredit - 100)
+    }
 
-    // }
+    const betFiveHundred = () => {
+        setCredit(prevCredit => prevCredit - 500)
+    }
+
+    const betThousand = () => {
+        setCredit(prevCredit => prevCredit - 1000)
+    }
 
     return (
         <Box className='container' p={3 / 2} mt={2} mb={1 / 2}>
@@ -29,7 +34,9 @@ const BetCard = () => {
                 <ThemeProvider theme={theme}>
                     <Button
                         variant='text'
-                        disableRipple={true}>You have Credit : <PaidIcon />{credit}
+                        disableRipple={true}
+                    >
+                        You have Credit : <PaidIcon />{(credit <= 0) ? 0 : credit}
                     </Button>
                 </ThemeProvider>
 
@@ -38,7 +45,8 @@ const BetCard = () => {
                     component='p'
                     fontFamily="'Bebas Neue', cursive"
                     letterSpacing='0.2rem'
-                    alignSelf='center'>
+                    alignSelf='center'
+                >
                     Place your Bet
                 </Typography>
 
@@ -47,25 +55,45 @@ const BetCard = () => {
                     aria-label="outlined button group"
                     color='secondary'>
                     <Button
-                        id='100'
-                        startIcon={<PaidIcon />}>
+                        startIcon={<PaidIcon />}
+                        onClick={betHundred}
+                    >
                         100
                     </Button>
                     <Button
-                        id='500'
-                        startIcon={<PaidIcon />}>
+                        startIcon={<PaidIcon />}
+                        onClick={betFiveHundred}
+                    >
                         500
                     </Button>
                     <Button
-                        id='1000'
+                        onClick={betThousand}
                         startIcon={<PaidIcon />}>
                         1000
                     </Button>
                 </ButtonGroup>
 
-                {/* <Typography variant='h6' component='p' fontFamily="'Bebas Neue', cursive" letterSpacing='0.2rem' alignSelf='center'>Your Bet</Typography>
+                {/* <Typography
+                    variant='h6'
+                    component='p'
+                    fontFamily="'Bebas Neue', cursive"
+                    letterSpacing='0.2rem'
+                    alignSelf='center'
+                >
+                    Your Bet
+                </Typography>
 
-                <Typography variant='h6' component='p' fontFamily="'Bebas Neue', cursive" letterSpacing='0.2rem' alignSelf='center'>You Won</Typography> */}
+                <br/>
+                
+                <Typography
+                    variant='h6'
+                    component='p'
+                    fontFamily="'Bebas Neue', cursive"
+                    letterSpacing='0.2rem'
+                    alignSelf='center'
+                >
+                    You Won
+                </Typography> */}
 
             </Stack>
         </Box>
